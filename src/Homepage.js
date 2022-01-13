af0f5e765117e26cfacbd678ce73dcd79ebf8f55
emmanuelhandsome1 <70865322+emmanuelhandsome1@users.noreply.github.com>
2022-01-13 13:08:20 +0100

    Add files via upload

---
+++ src/Homepage.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Homepage.scss'
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>CLOTHES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-items'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
